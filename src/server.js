c15a127f747bc3fb2cf66ef76bb53bd62515572a
blearn <blearn@psc.edu>
2019-05-08 17:30:46 -0400

    added enpoint for compute demo

--- src/server.js
+++ src/server.js
@@ -2,7 +2,10 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const request = require('request');
 const app = express();
+const { exec } = require('child_process');
 var myldap = require('./basicAuth.js');
+
+// App Server Configuration //
 
 const port = 8000;
 
@@ -12,17 +15,24 @@ app.use(bodyParser.json());
 app.set('views', 'templates');
 app.set('view engine', 'ejs');
 
+// for now, save user state in app (must change after demo)
+var app_state_user = "Portal Login";
+
+// GET requests for web pages //
+
 app.get('/', function (req, res){
     res.render('index', {error:""});
 });
 
 app.get('/login', function (req, res){
-    res.render('login', {error:"TEST"});
+    res.render('login', {error:""});
 });
 
 app.get('/portal', function (req, res){
-    res.render('portal', {error:""});
+    res.render('portal', {user:"Portal Login", stdout:""});
 });
+
+// POST requests for other API calls //
 
 app.post('/auth', function(req, res){
     myldap.init();
@@ -31,14 +41,27 @@ app.post('/auth', function(req, res){
     myldap.auth(usr, pass, function(passed){
         if(passed){
             console.log("LDAP Auth worked");
-            res.render('portal', {user: usr});
+            res.render('portal', {user: usr, stdout:""});
+            app_state_user = usr;
         }else{
             console.log("Failed to auth");
             res.render('login', {error: "failed to authenticate"});
         }
     });
-    
 });
+
+app.get('/compute', function(req, res){
+    exec('/usr/local/src/aurora/Job_Level0_Demo.sh', function(err, stdout, stderr){
+        if(err){
+            console.log(err);
+            res.render('portal', {user: app_state_user, stdout: "Job submission failed."});
+            return;
+        }
+        res.render('portal', {user: app_state_user, stdout: stdout});
+    });
+});
+
+// Start App Server //
 
 app.listen(port, function (){
     console.log('Server started. Listening on '+port);
